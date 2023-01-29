fead2ddbb8f5fff8038f6ce684851b662be04c12
Oliver Benns <oliverbenns@gmail.com>
2023-01-29 16:39:01 +0000

    Add wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,70 +8,32 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  Label,
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const pointCount = 8;
+
+const data = Array.from(Array(pointCount)).map((_, i) => {
+  return {
+    pv: i,
+  };
+});
 
 function App() {
   return (
-    <div className="App">
-      <div className="Inner">
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart width={300} height={100} data={data}>
-            <Line
-              type="monotone"
-              dataKey="pv"
-              stroke="#8884d8"
-              strokeWidth={2}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="1 1" />
+        <XAxis>
+          <Label>Play around</Label>
+        </XAxis>
+        <YAxis tickCount={pointCount} domain={[0, pointCount - 1]}>
+          <Label angle={-90}>Find out</Label>
+        </YAxis>
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
 
